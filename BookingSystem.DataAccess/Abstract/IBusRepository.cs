519a07f4ce366e46155d496f137d6fd17a705264
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-05-16 19:16:08 +0300

    Added bus repository

---
+++ BookingSystem.DataAccess/Abstract/IBusRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using BookingSystem.Entities;
+
+namespace BookingSystem.DataAccess.Abstract
+{
+    public interface IBusRepository
+    {
+        IEnumerable<Bus> Buses { get; }
+
+        void AddBus(Bus bus);
+        Bus RemovBus(Bus bus);
+    }
+}
---
+++ BookingSystem.DataAccess/Concrete/BusRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using BookingSystem.DataAccess.Abstract;
+using BookingSystem.Entities;
+
+namespace BookingSystem.DataAccess.Concrete
+{
+    public class BusRepository : IBusRepository
+    {
+        private readonly BookingSystemContext _context;
+        public IEnumerable<Bus> Buses => _context.Bus;
+
+        public BusRepository(BookingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBus(Bus bus)
+        {
+            if (bus == null)
+            {
+                throw new ArgumentNullException(nameof(bus), "Bus cannot be null");
+            }
+
+            _context.Bus.Add(bus);
+            _context.SaveChanges();
+        }
+
+        public Bus RemovBus(Bus bus)
+        {
+            if (bus == null)
+            {
+                throw new ArgumentNullException(nameof(bus), "Bus cannot be null");
+            }
+
+            var removableBus = _context.Bus.Find(bus);
+            if (removableBus != null)
+            {
+                _context.Bus.Remove(removableBus);
+                _context.SaveChanges();
+            }
+
+            return removableBus;
+        }
+    }
+}

1d63c1e5515cfa0e3ec74a6da30c45260421c62b
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-05-18 11:57:45 +0300

    Routes view model

--- BookingSystem.UI/ViewModels/RouteViewModel.cs
+++ BookingSystem.UI/ViewModels/RouteViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using BookingSystem.DataAccess.Concrete;
 using BookingSystem.Entities;
@@ -15,8 +17,13 @@ namespace BookingSystem.UI.ViewModels
         public ObservableCollection<Route> Routes { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private RoutePoint _rp2;
+
         private RelayCommand _addRoute;
         private RelayCommand _removeRoute;
+
+        public RoutePoint Rp1 { get; set; }
+        public RoutePoint Rp2 { get; set; }
 
         public RelayCommand AddRouteCommand
         {
@@ -83,12 +90,28 @@ namespace BookingSystem.UI.ViewModels
 
         public double? Length
         {
-            get => _selectedRoute.Length;
+            get => _selectedRoute?.Length;
             set
             {
                 _selectedRoute.Length = value;
                 OnPropertyChanged(nameof(Length));
             }
+        }
+
+        public IEnumerable<RoutePoint> RoutePoints
+        {
+            get
+            {
+                var routePoints = _unitOfWork.RoutePointRepository.RoutePoints.ToList();
+                Rp1 = routePoints.FirstOrDefault();
+                Rp2 = routePoints.FirstOrDefault();
+                return routePoints;
+            }
+        }
+
+        public RouteViewModel()
+        {
+            Routes = new ObservableCollection<Route>(_unitOfWork.RouteRepository.Routes);
         }
 
         [NotifyPropertyChangedInvocator]

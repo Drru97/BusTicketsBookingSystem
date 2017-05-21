82cd1732b117d673ecb559e4b906a4f5bd0c1e13
Oleg Dutsyak <oleg.dystak@gmail.com>
2017-05-21 22:53:23 +0300

    Added context changing while tabs navigate

--- BookingSystem.UI/MainWindow.xaml.cs
+++ BookingSystem.UI/MainWindow.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using BookingSystem.UI.ViewModels;
 
 namespace BookingSystem.UI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Schedule_Clicked(object sender, RoutedEventArgs e)
+        {
+            ManageJourneys.DataContext = new JourneyViewModel();
+        }
+
+        private void Routes_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            ManageRoutes.DataContext = new RouteViewModel();
         }
     }
 }
